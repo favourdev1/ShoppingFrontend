443305311c0028e68889e62ab6c1a520fdbbc9d0
favourdev1 <favourapps17@gmail.com>
2024-01-11 09:51:53 +0100

    added banner view

---
+++ pages/components/bannerview.php
@@ -0,0 +1,40 @@
+
+<div class="container mt-5">
+
+    <div class="row g-3 align-items-center justify-content-between">
+    <div class="col-md-2 text-start  border-dark px-auto">
+    <h2 class="mb-4">Better choices, better prices</h2>
+    </div>
+        <!-- First Column -->
+        <div class="col-md-2 text-center  border-dark px-auto">
+            <i class='bx bxs-dollar-circle' style='font-size:48px;'></i> 
+            <h6>Value-for-money</h6>
+            <p>We offer competitive prices on millions of items</p>
+        </div>
+
+        <!-- Second Column -->
+
+        <div class="col-md-2 text-center  border-dark px-auto">
+            <i class='bx bxs-truck' style='font-size:48px;'></i>
+            <h6>Fast delivery</h6>
+            <p>We offer a range of convenient delivery options</p>
+        </div>
+
+        <!-- Fourth Column -->
+        <div class="col-md-2 text-center  border-dark px-auto">
+            <i class='bx bxs-lock' style='font-size:48px;'></i>
+            <h6>Safe payments</h6>
+            <p>We protect your personal and financial information</p>
+        </div>
+
+        <!-- Fifth Column -->
+        <div class="col-md-2 text-center  border-dark px-auto">
+            <i class='bx bxs-shield' style='font-size:48px;'></i>
+            <h6>Buyer protection</h6>
+            <p>We guarantee a full refund if your order is not delivered</p>
+        </div>
+
+        
+
+    </div>
+</div>
