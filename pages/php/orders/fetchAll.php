86b97e83dc92ce322643209137b9d00ee4988d1f
Favourdev1 <favourapps17@gmail.com>
2024-05-30 09:52:54 +0100

    Refactor fetchAll.php to improve API URL handling

--- pages/php/orders/fetchAll.php
+++ pages/php/orders/fetchAll.php
@@ -3,9 +3,9 @@
 use Httpful\Request;
 
 $orderItems=[];
-echo $apiUrl . '/orders/';
+
 // Make a GET request to your API endpoint for a specific product
-$response = Request::get($apiUrl . '/orders/')
+$response = Request::get($apiUrl . '/orders')
     ->addHeaders($payloadRequest)
 
     ->send();
@@ -21,27 +21,24 @@ if ($statusCode === 200) {
     // Decode the JSON response
     $orderItems = json_decode(json_encode($responseData), true)['data'];
     // Print or use the retrieved product data
-    echo "<pre>";
+    // echo "<pre>";
 
-    print_r($orderItems);
+    // print_r($orderItems);
 
-    foreach($orderItems as $order){
-        print_r($order['product_id ']);
-    }
+    // foreach($orderItems as $order){
+    //     print_r($order['product_id ']);
+    // }
 
     
-    echo "</pre>";
+    // echo "</pre>";
 
-    die;
+    // die;
     
 } else {
-
-    $newLocation = $response->headers['location'];
-    echo "The API endpoint has been moved to: $newLocation\n"; 
     // $errorMessage = str_replace(',', '\n', $responseData->message);
     // $_SESSION['message'] = $errorMessage;
     // $_SESSION['status'] = "error";
-    print_r($responseData);
+    // echo $errorMessage;
     // header("Location: ../../products.php");
-    exit();
+    // exit();
 }
