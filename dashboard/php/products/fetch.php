07db8e60b1375b49c06e812d25b842aa773664b2
Favourdev1 <favourapps17@gmail.com>
2024-05-30 11:28:13 +0100

    Refactor fetch.php to fix API URL handling

--- dashboard/php/products/fetch.php
+++ dashboard/php/products/fetch.php
@@ -6,7 +6,8 @@ use Httpful\Request;
 if (isset($_GET['id'])) {
     // Specify the product ID you want to retrieve
     $productID = $_GET['id'];
-
+    echo $apiUrl . '/products' . $productID;
+    die;
     // Make a GET request to your API endpoint for a specific product
     $response = Request::get($apiUrl . '/products' . $productID)
         ->addHeaders([
